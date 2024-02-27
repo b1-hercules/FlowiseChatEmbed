13baec58f08dc6c2d81d79c2d6af6f82ebbb8086
Mohammad Iqbal Sugandhi <mohammad.iqbal@corp.bri.co.id>
2024-02-28 05:50:42 +0700

    add chatbot icon github

--- src/features/bubble/components/BubbleButton.tsx
+++ src/features/bubble/components/BubbleButton.tsx
@@ -30,7 +30,7 @@ export const BubbleButton = (props: Props) => {
         bottom: props.bottom ? `${props.bottom.toString()}px` : `${defaultBottom}px`,
       }}
     >
-      <Show when={isNotDefined(props.customIconSrc)} keyed>
+      {/* <Show when={isNotDefined(props.customIconSrc)} keyed>
         <svg
           viewBox="0 0 24 24"
           style={{
@@ -44,21 +44,19 @@ export const BubbleButton = (props: Props) => {
         >
           <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
         </svg>
-      </Show>
-      {/* <Show when={isNotDefined(props.customIconSrc)} keyed>
-        <img
-          src={iconImageUrl} // Change this path to where your image is located
-          class={
-            `absolute duration-200 transition ` +
-            (props.isBotOpened ? 'scale-0 opacity-0' : 'scale-100 opacity-100') +
-            (props.size === 'large' ? ' w-9 h-9' : ' w-7 h-7') // Adjust size as needed
-          }
-          style={{
-            stroke: props.iconColor ?? defaultIconColor, // This style may not be relevant for an image and can be adjusted or removed
-          }}
-          alt="Bubble button icon"
-        />
       </Show> */}
+      <Show when={isNotDefined(props.customIconSrc)} keyed>
+        <img
+          src="https://raw.githubusercontent.com/b1-hercules/FlowiseChatEmbed/main/src/assets/icon.png"
+          style={{
+            width: '200px', // Menetapkan lebar gambar
+            height: '200px', // Menetapkan tinggi gambar
+            position: 'absolute', // Memastikan gambar ditempatkan secara absolut
+          }}
+          class={`duration-200 transition ${props.isBotOpened ? 'scale-0 opacity-0' : 'scale-100 opacity-100'} ${props.size === 'large' ? ' w-9' : ' w-7'}`}
+          alt="Icon"
+        />
+      </Show>
       <Show when={props.customIconSrc}>
         <img
           src={props.customIconSrc}
